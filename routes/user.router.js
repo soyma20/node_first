541ab53822b3fcdaddb887ab9ca2fddc6707ee93
soyma20 <slaviksoyma2014@gmail.com>
2023-01-16 15:43:32 +0200

    feat: add validation for crud

--- routes/user.router.js
+++ routes/user.router.js
@@ -4,10 +4,10 @@ const userController = require("../controllers/user.controller");
 const userMiddleware = require("../middlewares/user.middleware");
 
 userRouter.get('/', userController.getAllUsers);
-userRouter.get('/:userId', userController.getUserById);
-userRouter.post('/', userMiddleware.checkUserOnCreate, userController.createUser);
-userRouter.put('/:userId', userController.updateUserById);
-userRouter.delete('/:userId', userController.deleteUserById);
+userRouter.get('/:userId',userMiddleware.isIdPresented, userController.getUserById);
+userRouter.post('/', userMiddleware.isNewUserValid, userMiddleware.isEmailRegistered, userController.createUser);
+userRouter.put('/:userId', userMiddleware.isIdPresented, userMiddleware.isUpdateUserValid,  userController.updateUserById);
+userRouter.delete('/:userId', userMiddleware.isIdPresented, userController.deleteUserById);
 
 
 module.exports = userRouter;
