deff78a6a0ff30cf269d4f1932f8608a289fd850
soyma20 <slaviksoyma2014@gmail.com>
2023-01-16 15:43:58 +0200

    feat: add validation for crud

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -1,18 +1,57 @@
 const CError = require("../error/CustomError");
 
+const userValidator = require("../validators/user.validator")
+const User = require("../dataBase/User")
+
 module.exports = {
-    checkUserOnCreate: (req, res, next) => {
+    isNewUserValid:(req, res, next)=>{
         try {
-            const {name = '', email = '', age = 0, password = ''} = req.body
-            if (!name || !email || !password){
-                throw new CError("Some filed is missing",400)
+            const {error, value} = userValidator.newUserValidator.validate(req.body);
+
+            if (error){
+                throw new CError(error.details[0].message, 400)
             }
-
-            next()
-
-        } catch (e) {
+            next();
+        }catch (e) {
             next(e)
         }
+    },
+    isEmailRegistered: async (req, res, next) => {
+        try {
+            const {email} = req.body;
+            const userByEmail = await User.findOne({email});
+            if (userByEmail){
+                throw new CError("User with email:" +email + "already exists",409)
+            }
+            next()
 
+        }catch (e) {
+            next(e)
+        }
+    },
+    isUpdateUserValid: (req, res, next)=>{
+        try {
+            const {error, value} = userValidator.updateUserValidator.validate(req.body);
+
+            if (error){
+                throw new CError(error.details[0].message, 400)
+            }
+            next();
+        }catch (e) {
+            next(e)
+        }
+    },
+    isIdPresented: async (req, res, next) => {
+        try {
+            const userId = req.params.userId;
+            const userById = await User.findById(userId);
+            if (!userById){
+                throw new CError("User does not exist")
+            }
+            next()
+        }catch (e) {
+            next(e)
+        }
     }
+
 }
