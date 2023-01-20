f1fc08d7dd5ba08d8737602f2acb6e5af74a0018
soyma20 <slaviksoyma2014@gmail.com>
2023-01-20 12:08:58 +0200

    feat: add validator for auth

---
+++ validators/auth.validator.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+const { emailValidator, passwordValidator} = require("./share");
+
+module.exports = {
+    login: Joi.object({
+        email: emailValidator.required(),
+        password: passwordValidator.required()
+    }),
+}
