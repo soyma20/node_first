2cd961b06c87ed936fc6cb7ecd40dc36cd20b36a
soyma20 <slaviksoyma2014@gmail.com>
2023-01-20 12:21:01 +0200

    feat: add secure endpoints that require access token

--- routes/user.router.js
+++ routes/user.router.js
@@ -2,12 +2,26 @@ const userRouter = require('express').Router();
 
 const userController = require("../controllers/user.controller");
 const userMiddleware = require("../middlewares/user.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
-userRouter.get('/', userController.getAllUsers);
-userRouter.get('/:userId',userMiddleware.isIdPresented, userController.getUserById);
-userRouter.post('/', userMiddleware.isNewUserValid, userMiddleware.isEmailRegistered, userController.createUser);
-userRouter.put('/:userId', userMiddleware.isIdPresented, userMiddleware.isUpdateUserValid,  userController.updateUserById);
-userRouter.delete('/:userId', userMiddleware.isIdPresented, userController.deleteUserById);
+userRouter.get('/',
+    userController.getAllUsers);
+userRouter.get('/:userId',
+    userMiddleware.isIdPresented,
+    userController.getUserById);
+userRouter.post('/',
+    userMiddleware.isNewUserValid,
+    userMiddleware.isEmailRegistered,
+    userController.createUser);
+userRouter.put('/:userId',
+    // userMiddleware.isIdPresented,
+    authMiddleware.checkAccessToken,
+    userMiddleware.isUpdateUserValid,
+    userController.updateUserById);
+userRouter.delete('/:userId',
+    userMiddleware.isIdPresented,
+    authMiddleware.checkAccessToken,
+    userController.deleteUserById);
 
 
 module.exports = userRouter;
