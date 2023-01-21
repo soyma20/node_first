4b77717980cf9e5bc6c7c9c704916fda8664126f
soyma20 <slaviksoyma2014@gmail.com>
2023-01-21 18:59:00 +0200

    feat: add email params and replace static values with process dotenv

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    ACCESS_TOKEN: 'wasasdas',
-    REFRESH_TOKEN: 'mamba',
-    PORT: 5000,
-    MONGO_URL : 'mongodb://127.0.0.1:27017/test'
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN || 'ba',
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN || 'la',
+
+    PORT: process.env.PORT || 3000,
+    MONGO_URL : process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/bala',
+
+    NO_REPLY_EMAIL : process.env.NO_REPLY_EMAIL || 'mail@mail.com',
+    NO_REPLY_EMAIL_PASSWORD : process.env.NO_REPLY_EMAIL_PASSWORD || 'mailer',
+
 }
