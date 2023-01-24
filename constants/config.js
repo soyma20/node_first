f389b29071a2610a3e4c6021e0a1107746d4b6c9
soyma20 <slaviksoyma2014@gmail.com>
2023-01-24 10:20:55 +0200

    feat: add FRONTEND_URL, FORGOT_PASSWORD_ACTION_TOKEN and ACTION_TOKEN_EXPIRES_IN

--- constants/config.js
+++ constants/config.js
@@ -2,10 +2,15 @@ module.exports = {
     ACCESS_TOKEN: process.env.ACCESS_TOKEN || 'ba',
     REFRESH_TOKEN: process.env.REFRESH_TOKEN || 'la',
 
+    FORGOT_PASSWORD_ACTION_TOKEN: process.env.FORGOT_PASSWORD_ACTION_TOKEN || 'Ga',
+
+    ACTION_TOKEN_EXPIRES_IN: process.env.ACTION_TOKEN_EXPIRES_IN || '15m',
+
     PORT: process.env.PORT || 3000,
-    MONGO_URL : process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/bala',
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/bala',
 
-    NO_REPLY_EMAIL : process.env.NO_REPLY_EMAIL || 'mail@mail.com',
-    NO_REPLY_EMAIL_PASSWORD : process.env.NO_REPLY_EMAIL_PASSWORD || 'mailer',
+    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || 'mail@mail.com',
+    NO_REPLY_EMAIL_PASSWORD: process.env.NO_REPLY_EMAIL_PASSWORD || 'mailer',
 
+    FRONTEND_URL: process.env.FRONTEND_URL || 'https://google.com'
 }
