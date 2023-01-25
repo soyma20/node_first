8f69879b3b971fad50495bb0d70bd44e23d33a41
soyma20 <slaviksoyma2014@gmail.com>
2023-01-25 17:28:32 +0200

    feat: add expires in for tokens

--- constants/config.js
+++ constants/config.js
@@ -5,6 +5,8 @@ module.exports = {
     FORGOT_PASSWORD_ACTION_TOKEN: process.env.FORGOT_PASSWORD_ACTION_TOKEN || 'Ga',
 
     ACTION_TOKEN_EXPIRES_IN: process.env.ACTION_TOKEN_EXPIRES_IN || '15m',
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN || '15m',
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN || '30m',
 
     PORT: process.env.PORT || 3000,
     MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/bala',
