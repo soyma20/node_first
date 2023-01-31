81b2c9c74304e9e8d6e5074bd4a98dab10f19b1d
soyma20 <slaviksoyma2014@gmail.com>
2023-01-31 10:11:04 +0200

    feat: add regex for PHONE, PASSWORD and EMAIL

--- constants/constant.js
+++ constants/constant.js
@@ -1,6 +1,10 @@
 module.exports = {
-    USER_REF : 'user',
+    USER_REF: 'user',
     OAUTH_REF: 'oauth',
     ACTION_TOKEN_REF: 'actionToken',
-    AUTHORIZATION:'Authorization'
+    AUTHORIZATION: 'Authorization',
+
+    PHONE_REGEX: /\(?\+[0-9]{1,3}\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})? ?(\w{1,10}\s?\d{1,6})?/,
+    PASSWORD_REGEX: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?[ #?!@$%^&*-]).{8,}$/,
+    EMAIL_REGEX: /^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/,
 }
