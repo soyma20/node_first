a97ef67e4c2f6b81d8b92c4197fec5690c197538
soyma20 <slaviksoyma2014@gmail.com>
2023-01-31 22:03:33 +0200

    feat: add expressFileUpload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express')
+const expressFileUpload = require('express-fileupload')
 const mongoose = require('mongoose');
 
 const userRouter = require('./routes/user.router')
@@ -14,6 +15,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+app.use(expressFileUpload())
 app.use('/auth', authRouter)
 app.use('/users', userRouter)
 
