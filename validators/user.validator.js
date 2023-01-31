db92c4b39d53663aad0d0524bfc35b9e1d2f0c57
soyma20 <slaviksoyma2014@gmail.com>
2023-01-31 10:12:53 +0200

    feat: add phone validator

--- validators/user.validator.js
+++ validators/user.validator.js
@@ -1,13 +1,14 @@
 const Joi = require('joi')
 
-const {emailValidator, nameValidator, ageValidator, passwordValidator} = require("./share");
+const {emailValidator, nameValidator, ageValidator, passwordValidator, phoneValidator} = require("./share");
 
 module.exports = {
     newUserValidator: Joi.object({
         name: nameValidator.required(),
         email: emailValidator.required(),
         age: ageValidator,
-        password: passwordValidator.required()
+        password: passwordValidator.required(),
+        phone: phoneValidator.required(),
     }),
 
     updateUserValidator: Joi.object({
