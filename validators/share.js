eabba96614c77f14dee86d6de684a542cb652188
soyma20 <slaviksoyma2014@gmail.com>
2023-01-31 10:11:53 +0200

    feat: add phone validator

--- validators/share.js
+++ validators/share.js
@@ -1,10 +1,11 @@
 const Joi = require("joi");
 
-
+const {EMAIL_REGEX,PHONE_REGEX,PASSWORD_REGEX}= require('../constants/constant');
 
 module.exports = {
     nameValidator: Joi.string().alphanum().min(2).max(20),
-    emailValidator: Joi.string().lowercase(),
+    emailValidator: Joi.string().lowercase().regex(EMAIL_REGEX),
     ageValidator: Joi.number().integer().min(15).max(130),
-    passwordValidator: Joi.string().required().min(5).max(25),
+    passwordValidator: Joi.string().required().min(8).max(25).regex(PASSWORD_REGEX),
+    phoneValidator: Joi.string().required().regex(PHONE_REGEX),
 }
